b1f883c56bf90d7602231255857a5ad0673cbd23
Xwilarg <xwilarg@yahoo.fr>
2020-07-11 01:51:51 +0200

    Add vocabulary parsing

--- JishoParser/Program.cs
+++ JishoParser/Program.cs
@@ -13,40 +13,117 @@ namespace JishoParser
     {
         static void Main(string[] args)
         {
-            if (!File.Exists("kanjis.txt"))
-                throw new FileNotFoundException();
-            Directory.CreateDirectory("img");
-            var content = File.ReadAllText("kanjis.txt").ToCharArray();
-            List<KanjiInfo> kanjis = new List<KanjiInfo>();
-            using (HttpClient hc = new HttpClient())
+            Console.WriteLine("Enter thing");
+            string str = Console.ReadLine();
+            if (str == "1")
             {
-                foreach (var elem in content)
+                if (!File.Exists("kanjis.txt"))
+                    throw new FileNotFoundException();
+                Directory.CreateDirectory("img");
+                var content = File.ReadAllText("kanjis.txt").ToCharArray();
+                List<KanjiInfo> kanjis = new List<KanjiInfo>();
+                using (HttpClient hc = new HttpClient())
                 {
-                    Console.Write("\rParsing " + elem);
-                    string html = hc.GetStringAsync("https://jisho.org/search/" + elem + "%20%23kanji").GetAwaiter().GetResult();
-                    kanjis.Add(new KanjiInfo
+                    foreach (var elem in content)
                     {
-                        kanji = elem,
-                        meaning = Regex.Match(html, "<div class=\"kanji-details__main-meanings\">([^<]+)<\\/div>").Groups[1].Value.Trim().Split(", "),
-                        onyomi = !html.Contains("<dt>On:") ? new string[0] : Regex.Matches(html.Split(new[] { "<dt>On:" }, StringSplitOptions.None)[1].Split(new[] { "</dd>" }, StringSplitOptions.None)[0], "<a[^>]+>([^<]+)<\\/a>").Cast<Match>().Select(x => x.Groups[1].Value).ToArray(),
-                        kunyomi = !html.Contains("<dt>Kun:") ? new string[0] : Regex.Matches(html.Split(new[] { "<dt>Kun:" }, StringSplitOptions.None)[1].Split(new string[] { "</dd>" }, StringSplitOptions.None)[0], "<a[^>]+>([^<]+)<\\/a>").Cast<Match>().Select(x => x.Groups[1].Value).ToArray(),
-                        imageId = ((int)elem).ToString()
-                    });
-                    File.WriteAllBytes("img/" + ((int)elem).ToString() + ".png", hc.GetByteArrayAsync("http://classic.jisho.org/static/images/stroke_diagrams/" + (int)elem + "_frames.png").GetAwaiter().GetResult());
+                        Console.Write("\rParsing " + elem);
+                        string html = hc.GetStringAsync("https://jisho.org/search/" + elem + "%20%23kanji").GetAwaiter().GetResult();
+                        var kunyomi = !html.Contains("<dt>Kun:") ? new string[0] : Regex.Matches(html.Split(new[] { "<dt>Kun:" }, StringSplitOptions.None)[1].Split(new string[] { "</dd>" }, StringSplitOptions.None)[0], "<a[^>]+>([^<]+)<\\/a>").Cast<Match>().Select(x => x.Groups[1].Value).ToArray();
+                        kanjis.Add(new KanjiInfo
+                        {
+                            kanji = elem + (kunyomi.Length > 1 && kunyomi[0].Contains(".") ? kunyomi[0].Split('.')[1] : ""),
+                            meaning = Regex.Match(html, "<div class=\"kanji-details__main-meanings\">([^<]+)<\\/div>").Groups[1].Value.Trim().Split(", "),
+                            onyomi = !html.Contains("<dt>On:") ? new string[0] : Regex.Matches(html.Split(new[] { "<dt>On:" }, StringSplitOptions.None)[1].Split(new[] { "</dd>" }, StringSplitOptions.None)[0], "<a[^>]+>([^<]+)<\\/a>").Cast<Match>().Select(x => x.Groups[1].Value).ToArray(),
+                            kunyomi = kunyomi,
+                            imageId = ((int)elem).ToString()
+                        });
+                        File.WriteAllBytes("img/" + ((int)elem).ToString() + ".png", hc.GetByteArrayAsync("http://classic.jisho.org/static/images/stroke_diagrams/" + (int)elem + "_frames.png").GetAwaiter().GetResult());
+                    }
                 }
+                File.WriteAllText("result.txt", JsonConvert.SerializeObject(kanjis));
             }
-            File.WriteAllText("result.txt", JsonConvert.SerializeObject(kanjis));
+            else if (str == "2")
+            {
+                Console.WriteLine("Enter jlpt level");
+                List<WordInfo> words = new List<WordInfo>();
+                string lvl = Console.ReadLine();
+                int i = 1;
+                while (true)
+                {
+                    using (HttpClient hc = new HttpClient())
+                    {
+                        string html = hc.GetStringAsync("https://jisho.org/search/%23jlpt-n" + lvl + "?page=" + i).GetAwaiter().GetResult();
+                        var coll = Regex.Matches(html, "<span class=\"text\">([^<]+)<\\/span>");
+                        if (coll.Count == 0)
+                            break;
+                        foreach (var elem in coll.Cast<Match>().Select(x => x.Groups[1].Value.Trim()))
+                        {
+                            Console.Write("\rParsing " + elem);
+                            dynamic json = JsonConvert.DeserializeObject(hc.GetStringAsync("https://jisho.org/api/v1/search/words?keyword=" + elem).GetAwaiter().GetResult());
+                            dynamic j = null;
+                            foreach (var tmp in json.data)
+                            {
+                                if (((JArray)tmp.jlpt).ToObject<string[]>().Contains("jlpt-n" + lvl))
+                                {
+                                    j = tmp;
+                                    break;
+                                }
+                            }
+                            if (j == null)
+                            {
+                                Console.WriteLine("Not found: " + elem);
+                                continue;
+                            }
+                            List<string> meaning = new List<string>();
+                            string reading = null;
+                            foreach (var m in j.senses)
+                            {
+                                meaning.AddRange(((JArray)m.english_definitions).ToObject<string[]>());
+                            }
+                            foreach (var r in j.japanese)
+                            {
+                                if (r.word == elem)
+                                {
+                                    reading = r.reading;
+                                }
+                            }
+                            if (reading == null)
+                            {
+                                Console.WriteLine("Invalid reading: " + elem);
+                                continue;
+                            }
+                            words.Add(new WordInfo
+                            {
+                                word = j.slug,
+                                meaning = meaning.ToArray(),
+                                reading = reading
+                            });
+                        }
+                    }
+                    i++;
+                }
+                File.WriteAllText("result.txt", JsonConvert.SerializeObject(words));
+            }
+            else
+                throw new ArgumentException();
             Console.WriteLine("Done");
             Console.ReadKey();
         }
 
         public class KanjiInfo
         {
-            public char kanji;
+            public string kanji;
             public string[] meaning;
             public string[] onyomi;
             public string[] kunyomi;
             public string imageId;
+        }
+
+        public class WordInfo
+        {
+            public string word;
+            public string[] meaning;
+            public string reading;
         }
     }
 }
